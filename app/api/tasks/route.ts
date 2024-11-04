2fae0641c04fe38d13ac335c1a2e381bd226f0b0
francis <mingomba@gmail.com>
2024-11-04 16:24:16 +0000

    Implement client-side validation

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,13 +1,7 @@
 
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
-
-
-const createTaskSchema = z.object({
-    title: z.string().min(1, 'Tilte is required.').max(255),
-    description: z.string().min(1, 'Description is required.')
-});
+import { createTaskSchema } from "../../validationSchema";
 
 
 export async function POST(request: NextRequest) {
