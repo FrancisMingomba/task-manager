bc2c97bbbcb3dedcaf984c9855cb8930cbe2383a
francis <mingomba@gmail.com>
2024-11-14 02:17:03 +0000

    Other commit

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,36 +1,45 @@
 import React from 'react'
 import { Button, Table } from '@radix-ui/themes';
 import Link from 'next/link';
-//import prisma from '@/prisma/client';
+//import prisma from '@prisma/client';
 
 const TasksPage = async () => {
 
- //const tasks = await prisma.issue.findMany();
+//const tasks = await prisma.Task.create;
+ 
 
   return (
     <div>
+      <div className='mb-5'>
       <Button>
         <Link href='/tasks/new'> New Task</Link>
       </Button>
-      <Table.Root>
+      </div>
+     
+      <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Task</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body>
         
             <Table.Row >
               <Table.Cell>Java</Table.Cell>
-              <Table.Cell>OPEN</Table.Cell>
-              <Table.Cell>11-8-2924</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>OPEN</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>11-8-2924</Table.Cell>
             </Table.Row>
             <Table.Row >
               <Table.Cell>C++</Table.Cell>
-              <Table.Cell>CLOSE</Table.Cell>
-              <Table.Cell>11-8-2924</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>IN-PROGRESS</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>11-8-2924</Table.Cell>
+            </Table.Row>
+            <Table.Row >
+              <Table.Cell>Python</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'> CLOSED</Table.Cell>
+              <Table.Cell className='hidden md:table-cell'>11-8-2924</Table.Cell>
             </Table.Row>
           
         </Table.Body>
