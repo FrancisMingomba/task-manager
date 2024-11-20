61d0542e53a4e8c9f78e334dd20e52b3135a36f3
francis <FrancisMingomba>
2024-11-20 16:54:47 +0000

    Edit details

---
+++ app/tasks/[id]/TaskDetails.tsx
@@ -0,0 +1,24 @@
+import TaskStatusBadge from '@/app/components/TaskStatusBadge';
+import { Task } from '@prisma/client';
+import { Heading, Flex, Card,Text } from '@radix-ui/themes';
+import React from 'react';
+import ReactMarkDown from 'react-markdown';
+
+
+const TaskDetails = ({ task }: { task: Task}) => {
+  return (
+
+    <>
+        <Heading>{task.title}</Heading>
+        <Flex className='space-x-3' my="2">
+            <TaskStatusBadge status={task.status} />
+            <Text>{task.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className='prose' mt="4">
+        <ReactMarkDown>{task.description}</ReactMarkDown>
+        </Card>
+    </>
+  )
+}
+
+export default TaskDetails
--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,10 +1,11 @@
 
-import TaskStatusBadge from '@/app/components/TaskStatusBadge'
-import prisma from '@/prisma/client'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
-import { notFound } from 'next/navigation'
-import React from 'react'
-import ReactMarkDown from 'react-markdown';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+
+import EditTaskButton from './EditTaskButton';
+import TaskDetails from './TaskDetails';
+
 
 interface Props {
     params: { id: string }
@@ -20,17 +21,14 @@ const TaskDetailPage = async ({ params }: Props) => {
         notFound();
 
   return (
-    <div>
-        <Heading>{task.title}</Heading>
-        <Flex className='space-x-3' my="2">
-            <TaskStatusBadge status={task.status} />
-            <Text>{task.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose' mt="4">
-          <ReactMarkDown>{task.description}</ReactMarkDown>
-        </Card>
-        
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+          <TaskDetails task={task} />
+      </Box>
+      <Box>
+          <EditTaskButton taskId={task.id} />
+      </Box>
+    </Grid>
   );
 };
 
