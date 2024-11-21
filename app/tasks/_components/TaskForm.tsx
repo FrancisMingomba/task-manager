083de8e8439bf4fc3c62b90db3cda6938217f287
francis <FrancisMingomba>
2024-11-21 00:19:35 +0000

    Edit details

---
+++ app/tasks/_components/TaskForm.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import "easymde/dist/easymde.min.css";
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod' ;
+import { createTaskSchema } from '@/app/validationSchema';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
+import { Task } from '@prisma/client';
+
+
+
+type TaskFormData = z.infer<typeof createTaskSchema>;
+
+const TaskForm = ({ task }: {task?: Task }) => {
+   const router = useRouter();
+   const {register, control, handleSubmit, formState: { errors} } = useForm<TaskFormData>({
+    resolver: zodResolver(createTaskSchema)
+   });
+   const [error, setError] =  useState('');
+   const [ isSubmitting, setSubmitting ] =  useState(false);
+
+   const onSubmit = handleSubmit(async ( data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/tasks',data)
+      router.push('/tasks');
+    } catch (error) {
+      setSubmitting(false);
+      setError('An unexpected error occured.');
+    }
+   
+   })
+
+  return (
+    <div className='max-w-xl'>
+      {error && <Callout.Root color="red" className='mb-5'>
+        <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>}
+    <form 
+       className=' space-y-3'
+       onSubmit={ onSubmit }>
+        <TextField.Root>
+            <TextField.Input defaultValue={task?.title} placeholder='Title'  {...register('title')}/>
+        </TextField.Root>
+      
+          <ErrorMessage>
+          {errors.title?.message}
+          </ErrorMessage>
+        
+        <Controller 
+        name='description'
+        control={control}
+        defaultValue={task?.description}
+        render={({ field }) => <SimpleMDE placeholder='Description'  {...field} /> }
+         />
+         <ErrorMessage>
+           {errors.description?.message}
+         </ErrorMessage>
+        <Button disabled={isSubmitting}>Submit New Issue  {isSubmitting && <Spinner />}</Button>
+    </form>
+    </div>
+  )
+}
+
+export default TaskForm
