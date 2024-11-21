9995fe83488fc271063338ec4419b0d8bc3a4544
francis <FrancisMingomba>
2024-11-21 02:27:30 +0000

    Build api to update task

---
+++ app/api/tasks/[id]/route.ts
@@ -0,0 +1,31 @@
+import { taskSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function  PATCH(
+    request: NextRequest, 
+    { params }: { params: {id: string}}) {
+        const body = await request.json();
+        const valadation = taskSchema.safeParse(body);
+        if (!valadation.success)
+            return NextResponse.json(valadation.error.format(), { status: 400 });
+
+        const task = await prisma.task.findUnique({
+            where: { id: parseInt(params.id)}
+        }) ;
+
+        if (!task)
+            return NextResponse.json({error: 'Invalid task' }, { status: 404});
+
+        const updateTask = await prisma.task.update({
+            where: { id: task.id}, 
+            data: {
+                title: body.title,
+                description: body.description
+            }
+        });
+            
+        return NextResponse.json(updateTask);
+
+    }
